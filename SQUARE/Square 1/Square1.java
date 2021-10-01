b8788e6ec5326b4c5e89c729cbd4e277db881d59
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-10-01 10:42:56 +0530

    Create Square1.java

---
+++ SQUARE/Square 1/Square1.java
@@ -0,0 +1,15 @@
+class Main {  
+  public static void main(String args[]) { 
+	  int i=0,j=0;
+	  while(i<5){
+		  while(j<5){
+			  System.out.print("* ");
+			  j++;
+		  }
+		  System.out.println();
+		  j=0;
+		  i++;
+	  }
+		
+  } 
+}
