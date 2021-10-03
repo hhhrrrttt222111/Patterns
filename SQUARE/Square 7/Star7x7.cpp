5df41d094ff5bf885dd4a46b2628e68a665c2aea
Vansh Gupta <91840861+vanshgupta-dev@users.noreply.github.com>
2021-10-03 22:53:30 +0530

    Start 7*7

---
+++ SQUARE/Square 7/Star7x7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1;i<=7;i++)
+    {
+        for(int j=1;j<=7;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
