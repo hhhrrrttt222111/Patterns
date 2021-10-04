d91735823026673fbdef50078dadfbd7c19ce438
Dhrubojeet Haldar <54771908+Starkl7@users.noreply.github.com>
2021-10-04 14:33:47 +0530

    Create square8.py

---
+++ SQUARE/Square 8/square8.py
@@ -0,0 +1,11 @@
+def main() :
+  #num = input()
+  num = 5
+  for i in range(num) :
+    for j in range(num) :
+      print(chr(64+num-i), end='')
+    print()
+
+
+if __name__ == "__main__":
+    main()
