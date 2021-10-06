601516a28319df9190e980f4cf3bf4b16322e471
Rupal1503 <rupal006btit20@igdtuw.ac.in>
2021-10-06 16:54:19 +0530

    Created a # square

---
+++ SQUARE/Square 11/Square11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// Created a # square pattern
+int main()
+{
+   
+    cout << "\n\n Print a pattern like square with # character:\n";
+    cout << "--------------------------------------------------\n";
+    
+    for (int row = 1; row <= 5; ++row) 
+    {
+        for (int col = 1; col <= 5; ++col) 
+        {
+            cout << "# ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
