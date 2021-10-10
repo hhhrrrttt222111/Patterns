778904c7cbc836d9bacc607f7168589d77dd4e8d
jgolling4697 <90636492+jgolling4697@users.noreply.github.com>
2021-10-10 18:55:20 -0400

    Added sandglass pattern in java

---
+++ Sandglass/sandglass.java
@@ -0,0 +1,47 @@
+package Sandglass;
+
+import java.util.Scanner;
+
+public class sandglass {
+
+    public static void main(String args[])
+    {
+        int row, i, j, space = 1;
+        System.out.print("enter a number of rows: ");
+        Scanner sc = new Scanner(System.in);
+
+        row = sc.nextInt();
+
+        space = 1;
+
+        for (j = 1; j<= row - 1; j++)
+        {
+            for (i = 1; i<= space; i++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for (i = 1; i<= 2 * (row - j) - 1; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        space = row - 1;
+        for (j = 1; j<= row; j++)
+        {
+            for (i = 1; i<= space; i++)
+            {
+                System.out.print(" ");
+            }
+            space--;
+            for (i = 1; i <= 2 * j - 1; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
