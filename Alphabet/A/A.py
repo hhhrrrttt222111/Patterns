bc8bc67492a9b749b8f71c3045335fe4da9792fe
SOMANATH-2039 <78495712+SOMANATH-2039@users.noreply.github.com>
2021-10-15 09:43:51 +0530

    Update A.py
    Removed semicolons from the program.

--- Alphabet/A/A.py
+++ Alphabet/A/A.py
@@ -1,4 +1,4 @@
-result_str="";    
+result_str=""    
 for row in range(0,7):    
     for column in range(0,7):     
         if (((column == 1 or column == 5) and row != 0) or ((row == 0 or row == 3) and (column > 1 and column < 5))):    
@@ -6,4 +6,4 @@ for row in range(0,7):
         else:      
             result_str=result_str+" "    
     result_str=result_str+"\n"    
-print(result_str);
+print(result_str)
