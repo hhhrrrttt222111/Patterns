9484842c1dfe7dadcef960df738c9b9e7f0e4cd1
buhari44 <adeyemibuari@gmail.com>
2021-10-22 19:15:05 +0100

    Added a pyramid pattern with javascript

---
+++ PYRAMID/Pyramid 01/pyramid.js
@@ -0,0 +1,15 @@
+let n = 5;
+let string = '';
+// External loop
+for (let i = 0; i < n; i++) {
+  // printing spaces
+  for (let j = 0; j < i; j++) {
+    string += ' ';
+  }
+  // printing star
+  for (let k = 0; k < 2 * (n - i) - 1; k++) {
+    string += '*';
+  }
+  string += '\n';
+}
+console.log(string);
