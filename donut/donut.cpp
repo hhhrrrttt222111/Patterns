af89fbcdfdd6da476d71ed1c6dcb5c4e3f85f95f
Gaurav Kar <72011979+grvkr777@users.noreply.github.com>
2021-10-28 15:41:00 +0530

    Create donut.cpp

---
+++ donut/donut.cpp
@@ -0,0 +1,52 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int k;
+double sin() ,cos();
+void delay(int number_of_seconds) 
+{ 
+    int milli_seconds = 1000 * number_of_seconds; 
+    clock_t start_time = clock(); 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+}
+int main(){
+    float A=0, B=0, i, j, z[1760];
+    char b[1760];
+    printf("[2J");
+    for(; ; ) {
+        delay(30);//delay can be changed as per requirement to speed up the trasitions
+        memset(b,32,1760);
+        memset(z,0,7040);
+        for(j=0; 6.28>j; j+=0.07) {
+            for(i=0; 6.28 >i; i+=0.02) {
+                float sini=sin(i),
+                      cosj=cos(j),
+                      sinA=sin(A),
+                      sinj=sin(j),
+                      cosA=cos(A),
+                      cosj2=cosj+2,
+                      mess=1/(sini*cosj2*sinA+sinj*cosA+5),
+                      cosi=cos(i),
+                      cosB=cos(B),
+                      sinB=sin(B),
+                      t=sini*cosj2*cosA-sinj* sinA;
+                int x=40+30*mess*(cosi*cosj2*cosB-t*sinB),
+                    y= 12+15*mess*(cosi*cosj2*sinB +t*cosB),
+                    o=x+80*y,
+                    N=8*((sinj*sinA-sini*cosj*cosA)*cosB-sini*cosj*sinA-sinj*cosA-cosi *cosj*sinB);
+                if(22>y&&y>0&&x>0&&80>x&&mess>z[o]){
+                    z[o]=mess;
+                    b[o]=".,-~:;=!*#$@"[N>0?N:0];
+                }
+            }
+        }
+        printf("[d");
+        for(k=0; 1761>k; k++)
+            putchar(k%80?b[k]:10);
+        A+=0.04;
+        B+= 0.02;
+    }
+}
