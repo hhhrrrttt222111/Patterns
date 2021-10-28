039ebf7a49a45e52d4410ac4d61d72242b780a4e
Manish57-droid <kushwahamanish572000@gmail.com>
2021-10-28 19:28:54 +0530

    Create trishul.cpp

---
+++ Trishul/trishul.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{int i,j;
+
+cout<<"                    **      **"<<endl;
+for (int i = 0; i < 2; i++)
+{
+    cout<<"                    **  **  **"<<endl;
+}
+    cout<<"                      ******"<<endl ;
+for ( j = 0; j < 5; j++)
+{
+    cout<<"                        **   "<<endl;
+}
+return 0;
+}
