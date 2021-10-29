3df91013ceef62918d89b90aeca01f0c160651e9
Parjanya HK <61843804+parjanyahk@users.noreply.github.com>
2021-10-30 01:19:55 +0530

    Create invertedPyramid.py

---
+++ PYRAMID/Pyramid 21/invertedPyramid.py
@@ -0,0 +1,10 @@
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 1, -1):
+    for space in range(0, rows-i):
+        print("  ", end="")
+    for j in range(i, 2*i-1):
+        print("* ", end="")
+    for j in range(1, i-1):
+        print("* ", end="")
+    print()
